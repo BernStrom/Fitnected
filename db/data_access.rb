32b322dadfd45c93f7ab8ef20dfd6d3d56c62e4f
Bern <bernnystrom@gmail.com>
2020-10-27 03:10:35 +0800

    Impletment basic CRUD functionalities and styles

--- db/data_access.rb
+++ db/data_access.rb
@@ -20,9 +20,19 @@ def find_user_by_firstname first_name
     results[0]
 end
 
-def create_user first_name, last_name, email, password_digest, fitness_goal, current_weight, goal_weight
-    sql = "INSERT INTO users (first_name, last_name, email, password_digest, fitness_goal, current_weight, goal_weight) VALUES ($1, $2, $3, $4, $5, $6, $7);"
-    run_sql(sql, [first_name, last_name, email, password_digest, fitness_goal, current_weight, goal_weight])
+def create_user first_name, last_name, email, password_digest, fitness_goal, current_weight, goal_weight, avatar_url
+    sql = "INSERT INTO users (first_name, last_name, email, password_digest, fitness_goal, current_weight, goal_weight, avatar_url) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);"
+    run_sql(sql, [first_name, last_name, email, password_digest, fitness_goal, current_weight, goal_weight, avatar_url])
+end
+
+def update_user first_name, last_name, email, fitness_goal, current_weight, goal_weight, avatar_url, id
+    sql = "UPDATE users SET first_name = $1, last_name = $2, email = $3, fitness_goal = $4, current_weight = $5, goal_weight = $6, avatar_url = $7 WHERE id = $8;"
+    run_sql(sql, [first_name, last_name, email, fitness_goal, current_weight, goal_weight, avatar_url, id])
+end
+
+def delete_user id
+    sql = "DELETE FROM users WHERE id = $1;"
+    run_sql(sql, [id])
 end
 
 def logged_in_user
--- main.rb
+++ main.rb
@@ -33,9 +33,9 @@ end
 
 # INSERTS the newly registered user details into the db
 post '/register' do
-  password_digest = BCrypt::Password.create(params['password_digest'])
+  password_digest = BCrypt::Password.create(params['password'])
 
-  create_user(params['first_name'], params['last_name'], params['email'], params['#{password_digest}'], params['fitness_goal'], params['current_weight'], params['goal_weight'], params['avatar_url'])
+  create_user(params['first_name'], params['last_name'], params['email'], params['password_digest'], params['fitness_goal'], params['current_weight'], params['goal_weight'], params['avatar_url'])
   redirect "/"
 end
 
@@ -63,7 +63,47 @@ delete '/logout' do
 end
 
 # Directs to the user profile page
-get '/fitnected/:first_name' do
+get '/profile' do
+  redirect "/login" unless logged_in?
   user = logged_in_user
-  erb :user, locals: { user: user}
+  erb :profile, locals: { user: user}
+end
+
+get '/profile/:id/edit' do
+  user = find_user_by_id(params['id'])
+  erb :edit, locals: {user: user}
+end
+
+delete '/profile/:id' do
+  delete_user(params['id'])
+  redirect "/"
+end
+
+patch '/profile/:id' do
+  update_user(params['first_name'], params['last_name'], params['email'], params['fitness_goal'], params['current_weight'], params['goal_weight'], params['avatar_url'], params['id'])
+  redirect "/profile"
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/exercises' do
+  redirect '/login' unless logged_in?
+  user = logged_in_user
+  erb :exercises, locals: { user: user}
+end
+
+get '/diet' do
+  redirect '/login' unless logged_in?
+  user = logged_in_user
+  erb :diet, locals: { user: user}
+end
+
+get '/privacy' do
+  erb :privacy
+end
+
+get '/contact' do
+  erb :contact
 end
